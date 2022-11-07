2c08ee96bbf041b6508ead015cd3f7e3159a1357
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2022-11-07 19:45:33 -0300

    Add files via upload

---
+++ 1019/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hora, min, seg, resto;
+            string[] vetor = Console.ReadLine().Split(' ');
+            int N = int.Parse(vetor[0]);
+
+            hora=N/3600;
+            resto=N%3600;
+
+            min=resto/60;
+            seg=resto%60;
+            
+            Console.WriteLine(hora+":"+min+":"+seg);
+
+        }
+    }
+}
---
+++ 1020/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anos, mes, dias, resto;
+            string[] vet = Console.ReadLine().Split();
+            int idade = int.Parse(vet[0]);
+
+            anos = idade/365;
+            resto= idade%365;
+            mes = resto/30;
+            dias = resto%30;
+
+            Console.WriteLine(anos+" ano(s)");
+            Console.WriteLine(mes+" mes(es)");
+            Console.WriteLine(dias+" dia(s)");
+
+        }
+    }
+}
