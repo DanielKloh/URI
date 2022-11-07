c0c49830d8ff76b513d9c61852566b4e0d962936
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2022-11-07 19:34:16 -0300

    Add files via upload

---
+++ 1002/1002/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaro varaibel
+            // leitura do teclado
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            // calculo
+            double area = 3.14159 * (raio * raio);
+
+            // impressao do resultado
+            // 4 casas decimais
+            // com ponto
+            Console.WriteLine("A=" + area.ToString("F4",CultureInfo.InvariantCulture));
+        }
+    }
+}
