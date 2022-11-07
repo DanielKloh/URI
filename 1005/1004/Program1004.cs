c8ec2ff83d5c8d77d3f771c7e3b0be5445ee0c75
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2022-11-07 19:39:31 -0300

    Add files via upload

---
+++ 1005/1004/Program1004.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            var PROD =A*B;
+
+            Console.WriteLine("PROD = "+PROD);
+        }
+    }
+}
