148f7b1c665c151860cc95f04ff499302e49a97d
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2022-11-07 19:53:37 -0300

    Add files via upload

---
+++ 1051/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1051
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sal = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            if(sal<=2000){
+                Console.WriteLine("Isento");
+            }
+            if(sal>2000 && sal<=3000){
+                double salf=(sal-2000)*0.08;
+                Console.WriteLine("R$ "+salf.ToString("F2",CultureInfo.InvariantCulture));
+            }
+            if(sal>3000 && sal<=4500){
+                double salf=(sal-3000)*0.18+80;
+                Console.WriteLine("R$ "+salf.ToString("F2",CultureInfo.InvariantCulture));
+            }
+            if(sal>4500){
+                double salf=(sal-4500)*0.28+80+270;
+                Console.WriteLine("R$ "+salf.ToString("F2",CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
