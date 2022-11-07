9ce93610930201d4fd908bc8526eae80bcac54c5
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2022-11-07 19:41:23 -0300

    Add files via upload

---
+++ 1008/Program1008.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NUMBER = int.Parse(Console.ReadLine());
+            int HORATRABALHO = int.Parse(Console.ReadLine());
+            double SALARIOHORA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double SALARY = HORATRABALHO * SALARIOHORA;
+
+            Console.WriteLine("NUMBER = " + NUMBER);
+            Console.WriteLine("SALARY = U$ " + SALARY.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ 1009/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1009v
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = Console.ReadLine();
+            double sal = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double vendas = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            double comis = (vendas*0.15)+sal;
+
+            Console.WriteLine("TOTAL = R$ "+comis.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
