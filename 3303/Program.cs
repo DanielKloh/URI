f3188b07e2164034b8f6d75bb832f6485418c76f
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2022-11-07 21:48:05 -0300

    Add files via upload

---
+++ 3303/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3303
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont = 0;
+            string texto = Console.ReadLine();
+
+            foreach(char v in texto){
+                cont += 1;
+            }
+            if(cont>=10){
+                Console.WriteLine("palavrao");
+            }
+            else{
+                Console.WriteLine("palavrinha");
+            }
+        }
+    }
+}
