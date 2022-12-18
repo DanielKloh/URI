8a23cdba58e02ee567cc64202569a4dc146e1c63
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2022-12-18 11:36:24 -0300

    Add files via upload

---
+++ 3346/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace _3346
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaração de variavel
+            double conta = 0;
+
+            //entrada de valores
+            string[] vetor = Console.ReadLine().Split();
+            double PIB1 = double.Parse(vetor[0],CultureInfo.InvariantCulture);
+            double PIB2 = double.Parse(vetor[1],CultureInfo.InvariantCulture);            
+
+            //formula
+            conta = ((((1.0 + PIB1/100.00) * (1.0 + PIB2/100.00)) - 1.0) * 100.0);
+
+            //exibição de valores
+            Console.WriteLine(conta.ToString("F6",CultureInfo.InvariantCulture));
+        }
+    }
+}
