746f6cf3aa1618d23cbec4355f1780db44acb56d
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2023-01-06 22:11:22 -0300

    Add files via upload

---
+++ 3342/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _3342
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variavel
+            double casas = 0;
+            double casa_branca = 0;
+            double casas_preta = 0;
+
+            //Entrada de dados
+            double tabuleiro = double.Parse(Console.ReadLine());
+
+            //Calculo de quadrados do tabuleiro
+            casas = tabuleiro * tabuleiro;
+
+            //se for par ou impar
+            if(casas %2 == 0 ){
+                casa_branca = casas / 2;
+                casas_preta = casas / 2;
+
+                Console.WriteLine(casa_branca+" casas brancas e "+ casas_preta +" casas pretas");
+            }
+
+            else{
+                casas_preta = Math.Round(casas/2);
+                casa_branca = casas_preta + 1;
+                Console.WriteLine(casa_branca+" casas brancas e "+ casas_preta +" casas pretas");
+            }
+        }
+    }
+}
