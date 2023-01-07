67729d4f78770391b81be19ab7400d42584e66bf
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2023-01-06 22:11:47 -0300

    Add files via upload

---
+++ 3344/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _3342
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("4");
+        }
+    }
+}
