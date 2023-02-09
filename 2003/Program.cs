53794ce791b3885761922a05de1e389d9529e0d3
DanielKloh <103609213+DanielKloh@users.noreply.github.com>
2023-02-09 16:06:39 -0300

    Add files via upload

---
+++ 2003/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _2003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                do
+                {
+                    string[] vetor = Console.ReadLine().Split(':');
+
+                    int h = int.Parse(vetor[0]);
+                    int m = int.Parse(vetor[1]);
+
+                    if (h < 7)
+                    {
+                        m = 0;
+                        Console.WriteLine("Atraso maximo: " + m);
+                    }
+
+                    else if (h == 7)
+                    {
+                        Console.WriteLine("Atraso maximo: " + m);
+                    }
+
+                    if (h == 8)
+                    {
+                        m += 60;
+                        Console.WriteLine("Atraso maximo: " + m);
+                    }
+
+                    else if( h==9)
+                    {
+                        m += 120;
+                        Console.WriteLine("Atraso maximo: " + m);
+                    }
+                    
+                } while (true);
+
+            }
+            catch (System.Exception)
+            {
+                return;
+                throw;
+            }
+        }
+    }
+}
---
+++ 2523/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _2523
+{
+    class Program
+    {
+        static void Main(string[] args){
+
+            do
+            {
+                int controle = 0;
+
+                string alfabeto = Console.ReadLine();
+
+
+                if(string.IsNullOrEmpty(alfabeto)){
+                    break;
+                }
+
+                int num = int.Parse(Console.ReadLine());
+
+                string[] str = Console.ReadLine().Split();
+                int[] posicoes = new int[num];
+
+                for (int i = 0; i < num; i++)
+                {
+                    posicoes[i] = int.Parse(str[i]);
+                }
+
+                for (int i = 0; i < num; i++)
+                {
+                    foreach (char c in alfabeto)
+                    {
+                        controle += 1;
+
+                        if (controle == posicoes[i])
+                        {
+                            Console.Write(c);
+                        }
+                    }
+                    controle = 0;
+                }
+                Console.WriteLine();
+
+            } while (true);
+        }
+    }
+}
